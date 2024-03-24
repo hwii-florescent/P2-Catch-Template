a927993660c36d6b14978298b189ec02c1a74e72
hwii <116895905+hwii-florescent@users.noreply.github.com>
2024-03-24 20:27:17 +0000

    Extra credit

--- src/AdjacencyList.h
+++ src/AdjacencyList.h
@@ -1,15 +1,34 @@
-#pragma once
-
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 #include <string>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
 class AdjacencyList {
-    private:
-    //Think about what member variables you need to initialize
-    public:
-    //Think about what helper functions you will need in the algorithm
-    string PageRank(int n);
-};
+public:
+    //adds an edge from 'from' to 'to' in the graph
+    void addEdge(const string& from, const string& to);
 
-// This class and method are optional.
+    //calculates the PageRank for each page after a specified number of iterations
+    string PageRank(int iterations);
+
+    //returns a string representing the PageRank of each page, sorted alphabetically
+    string getRanksAsString() const;
+
+    //parse the input and calculate the rank
+    void parseInput(const std::string& input);
+
+private:
+    //adjacency list representation of the graph with the webpage as key and the value is a list of webpages it points to.
+    unordered_map<string, vector<string>> adjList;
+
+    //current PageRank of each page with webpage as the key and its rank as the value
+    unordered_map<string, double> ranks;
+
+    //initializes the ranks of all pages
+    void initializeRanks();
+};
