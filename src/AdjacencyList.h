96fec8a5f6e5bcf1a9f6829031564caef025d677
hwii <116895905+hwii-florescent@users.noreply.github.com>
2024-04-04 01:19:10 +0000

    initial commit

--- src/AdjacencyList.h
+++ src/AdjacencyList.h
@@ -1,34 +1,144 @@
+// Copyright [2023] <Tuan Truong & Lam Luu>
+#ifndef AUTOGRADER_SOURCE_TESTS_COP3530_6_1_H
+#define AUTOGRADER_SOURCE_TESTS_COP3530_6_1_H
 #include <iostream>
-#include <unordered_map>
-#include <vector>
 #include <string>
-#include <algorithm>
-#include <iomanip>
-#include <sstream>
+#include <vector>
+#include <unordered_map>
 
-using namespace std;
+class Graph
+{
+   private:
+      // TODO: Graph structure that supports parallel edges and self-loops
+      std::unordered_map<int, std::vector<std::pair<int, int>>> table;
 
-class AdjacencyList {
-public:
-    //adds an edge from 'from' to 'to' in the graph
-    void addEdge(const string& from, const string& to);
-
-    //calculates the PageRank for each page after a specified number of iterations
-    string PageRank(int iterations);
-
-    //returns a string representing the PageRank of each page, sorted alphabetically
-    string getRanksAsString() const;
-
-    //parse the input and calculate the rank
-    void parseInput(const std::string& input);
-
-private:
-    //adjacency list representation of the graph with the webpage as key and the value is a list of webpages it points to.
-    unordered_map<string, vector<string>> adjList;
-
-    //current PageRank of each page with webpage as the key and its rank as the value
-    unordered_map<string, double> ranks;
-
-    //initializes the ranks of all pages
-    void initializeRanks();
+    public:
+      void insertEdge(int from, int to, int weight);
+      bool isEdge(int from, int to);
+      int sumEdge(int from, int to);
+      std::vector<int> getWeight(int from, int to);
+      std::vector<int> getAdjacent(int vertex);
 };
+
+void Graph::insertEdge(int from, int to, int weight)
+{
+    std::vector<std::pair<int, int>> that_vector;
+    std::pair<int, int> result(to, weight);
+   /*
+        TODO: insertEdge() adds a new edge between the from
+        and to vertex.
+   */
+
+    if (!table.count(from))
+    {
+        table[from] = that_vector;
+        table[from].push_back(result);
+    }
+    else
+    {
+        table[from].push_back(result);
+    }
+}
+
+bool Graph::isEdge(int from, int to)
+{
+    /*
+        TODO: isEdge() returns a boolean indicating true
+        if there is an edge between the from and to vertex
+    */
+
+    for (auto member: table[from])
+    {
+        if (member.first == to)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int Graph::sumEdge(int from, int to)
+{
+    int sum = 0;
+
+    /*
+        TODO: sumEdge() returns the sum of weights of all edges
+        connecting the from and to vertex. Returns 0 if no edges
+        connect the two vertices.
+    */
+
+    for (auto member: table[from])
+    {
+        if (member.first == to)
+        {
+            sum += member.second;
+        }
+    }
+
+    for (auto member: table[to])
+    {
+        if (member.first == from)
+        {
+            if (member.first == to) continue;
+            else sum += member.second;
+        }
+    }
+
+    return sum;
+}
+
+std::vector<int> Graph::getWeight(int from, int to)
+{
+    std::vector<int> sorted;
+
+    for (auto member: table[from])
+    {
+        if (member.first == to)
+        {
+            sorted.push_back(member.second);
+        }
+    }
+
+    for (auto member: table[to])
+    {
+        if (member.first == from)
+        {
+            if (member.second > sorted.at(0))
+            {
+                sorted.push_back(member.second);
+            }
+            else
+            {
+                sorted.push_back(member.second);
+                int temp = sorted.at(0);
+                sorted.at(0) = sorted.at(1);
+                sorted.at(1) = temp;
+            }
+        }
+    }
+    
+    /*
+        TODO: getWeight() returns a sorted vector containing all
+        weights of the edges connecting the from and to vertex
+    */
+    return sorted;
+}
+
+std::vector<int> Graph::getAdjacent(int vertex)
+{
+    std::vector<int> adjacent;
+
+    for (auto member: table[vertex])
+    {
+        adjacent.push_back(member.first);
+    }
+    
+    /*
+        TODO: getAdjacent() returns a sorted vector of all vertices
+        that are connected to a vertex
+    */
+    return adjacent;
+}
+
+#endif // AUTOGRADER_SOURCE_TESTS_COP3530_6_1_H"
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,95 @@
 #include <iostream>
-
-#include "AdjacencyList.h"
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
+class AdjacencyList {
+private:
+//Think about what member variables you need to initialize
+map<string, vector<string>> inDegree;
+map<string, float> outDegree;
+map<string, float> pageRank;
+
+public:
+//Think about what helper functions you will need in the algorithm
+    void PageRank(int n);
+    void insert(const string& from, const string& to);
+};
+
+void AdjacencyList::insert(const string& _from, const string& _to){
+        inDegree[_to].push_back(_from);
+        outDegree[_from] += 1;
+
+        if(inDegree.find(_from) == inDegree.end()){
+            inDegree[_from] = {};
+        }
+        if(outDegree.find(_to) == outDegree.end()){
+            outDegree[_to] = {};
+        }
+}
+
+
+
+void AdjacencyList::PageRank(int n){
+// Initialize Map
+    map<string, float> tempPageRank;
+
+    for(auto & i : inDegree){
+        pageRank[i.first] = (1.0f / inDegree.size());
+    }
+
+    // Power iterator
+    for(int i = 1; i < n; ++i){
+        tempPageRank = pageRank;
+        // Go through each vertex and its indegrees
+        for(auto & j: inDegree){
+
+            float rankSum = 0;
+            // iterate through array of vertexes
+/*
+            for(int k = 0; k < j.second.size(); ++i){
+                rankSum += (1.0f / outDegree.find(j.second[k])->second) * pageRank[j.second[k]];
+            }
+ */
+
+            for(auto & k: j.second){
+                rankSum += (1.0f / (float)outDegree.find(k)->second) * pageRank[k];
+            }
+
+
+            tempPageRank[j.first] = rankSum;
+
+
+        }
+        pageRank = tempPageRank;
+    }
+
+    for(auto & i : pageRank){
+        cout << setprecision(2) << fixed << i.first << " " << i.second << endl;
+    }
+
+
+}
+
+
+int main()
+{
+    AdjacencyList simplePageRank;
     int no_of_lines, power_iterations;
-    string from, to;
-    cin >> no_of_lines;
-    cin >> power_iterations;
-    for (int i = 0; i < no_of_lines; i++) {
-        cin >> from;
-        cin >> to;
-        // Do Something
+    std::string from, to;
+    std::cin >> no_of_lines;
+    std::cin >> power_iterations;
+    for(int i = 0; i < no_of_lines; i++)
+    {
+        std::cin >> from;
+        std::cin >> to;
+        simplePageRank.insert(from, to);
+
     }
     //Create a graph object
-    // Created_Graph.PageRank(power_iterations);}
+    simplePageRank.PageRank(power_iterations);
 }
